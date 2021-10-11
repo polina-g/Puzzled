122dbf9fec77cdded3dfd02d658da94ace7a12dc
polina-g <ignatieva.polina@gmail.com>
2021-10-11 13:52:13 -0400

    add: add exchange puzzles to user inventory.

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const userRouter = require('express').Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user.js');
 const Puzzle = require('../models/puzzle.js');
+const helper = require('../helpers/middleware.js')
 //=============================================================================
 //ROUTES
 //=============================================================================
@@ -26,5 +27,32 @@ userRouter.post('/signup', async (req, res) => {
         console.log('something went wrong when adding registration ', + error);
     }
 });
+//==========================ADD PUZZLE TO EXCHANGE INVENTORY===================
+userRouter.put('/:id/add', helper.isAuthenticated, helper.findUser, async (req, res) => {
+    try {
+    //TODO
+        //Find relevant puzzle from req.param.id
+        const puzzle = await  Puzzle.findById(req.params.id);
+        //Change isAvailable to false in the puzzle
+        puzzle.isAvailable = false;
+        puzzle.save();
+        //Update User model to add puzzle object in the puzzle array
+        req.user.exchange_inventory.push(puzzle);
+        req.user.save();
+        //Redirect to show all inventory
+        res.redirect('/inventory');
+    } catch (error) {
+        console.log('Somethng went wrong adding puzzle to exchange inventory! Error: ', error);
+    }
+
+
+});
+//==========================ADD PUZZLE TO EXCHANGE INVENTORY===================
+userRouter.get('/inventory', helper.isAuthenticated, helper.findUser, (req, res) => {
+    res.render('./users/inventory.ejs');
+})
+
+
+
 
 module.exports = userRouter;
